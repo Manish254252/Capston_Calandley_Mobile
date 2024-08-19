d03d1d4996a2a12b6764eeb02ec3fdb09973a3e3
ManishSolanki.Khimjibhai <ManishSolanki.Khimjibhai@ust.com>
2024-08-19 17:56:53 +0530

    Initial commit

---
+++ src/test/java/com/automation/pages/BasePage.java
@@ -0,0 +1,139 @@
+package com.automation.pages;
+
+import com.automation.utils.DriverManager;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.PerformsTouchActions;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Pause;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+import static io.appium.java_client.touch.WaitOptions.waitOptions;
+import static io.appium.java_client.touch.offset.PointOption.point;
+import static java.time.Duration.ofMillis;
+import static java.time.Duration.ofSeconds;
+
+public class BasePage {
+
+    static AppiumDriver driver;
+    WebDriverWait wait;
+
+    BasePage() {
+        driver = DriverManager.getDriver();
+        PageFactory.initElements(driver, this);
+        wait = new WebDriverWait(driver, ofSeconds(10));
+    }
+
+    public boolean isPresentWithoutWait(WebElement element) {
+        try {
+           setImplicitWait(0);
+           element.isDisplayed();
+            System.out.println("true");
+            return element.isDisplayed();
+        } catch (Exception e) {
+            System.out.println("false");
+            return false;
+        }finally {
+            setImplicitWait(60);
+        }
+    }
+    public void setImplicitWait(long timeInSeconds)
+    {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(timeInSeconds));
+    }
+    public boolean isPresent(WebElement element) {
+        try {
+            wait.until(ExpectedConditions.visibilityOf(element));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void waitForElementToBeVisible(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void waitForElementToBeClickable(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public void clickOnElementIfPresent(WebElement element) {
+        if (isPresent(element)) {
+            element.click();
+        }
+    }
+    public void tap(int x, int y) {
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        Sequence tap = new Sequence(finger, 1);
+
+        tap.addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), x, y));
+        tap.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        tap.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        driver.perform(List.of(tap));
+    }
+
+    public void scrollOrSwipe(int startX, int startY, int endX, int endY) {
+
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+
+
+        Sequence scroll = new Sequence(finger, 1);
+
+
+        scroll.addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, startY));
+        scroll.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        scroll.addAction(new Pause(finger,Duration.ofSeconds(1)));
+        scroll.addAction(finger.createPointerMove(Duration.ofSeconds(1), PointerInput.Origin.viewport(), endX, endY));
+        scroll.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+
+        driver.perform(Arrays.asList(scroll));
+    }
+
+    public void zoom(WebElement element , int zoom) {
+
+
+        int width = element.getSize().getWidth();
+        int height = element.getSize().getHeight();
+        int x =element.getLocation().getX()+ width/2;
+        int y = element.getLocation().getY()+height/2;
+        PointerInput finger1 = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+        PointerInput finger2 = new PointerInput(PointerInput.Kind.TOUCH, "finger2");
+
+
+        Sequence zoomInFinger1 = new Sequence(finger1, 1);
+        Sequence zoomInFinger2 = new Sequence(finger2, 1);
+
+
+        zoomInFinger1.addAction(finger1.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), x,y));
+        zoomInFinger1.addAction(finger1.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        zoomInFinger1.addAction(new Pause(finger1, Duration.ofMillis(100)));
+        zoomInFinger1.addAction(finger1.createPointerMove(Duration.ofSeconds(1), PointerInput.Origin.viewport(), x+zoom, y+zoom));
+        zoomInFinger1.addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+
+        zoomInFinger2.addAction(finger2.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), x, y));
+        zoomInFinger2.addAction(finger2.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        zoomInFinger2.addAction(new Pause(finger2, Duration.ofMillis(100)));
+        zoomInFinger2.addAction(finger2.createPointerMove(Duration.ofSeconds(1), PointerInput.Origin.viewport(), x-zoom, y-zoom));
+        zoomInFinger2.addAction(finger2.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+
+        driver.perform(Arrays.asList(zoomInFinger1, zoomInFinger2));
+    }
+
+
+}
+
+
